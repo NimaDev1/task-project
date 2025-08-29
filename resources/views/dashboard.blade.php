89ce858bdf0e95545dc92d087dad1a1355c4acb3
NimaDev1 <softer91.uz@gmail.com>
2025-08-29 17:35:08 +0500

    application sending and list is done

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,10 +5,10 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+    <div class="py-10">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-6">
+            <div class="bg-white dark:bg-gray-900 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="text-gray-100 dark:text-gray-100">
 
                     @if (auth()->user()->role->name == 'manager')
                         <span class="text-bold text-blue-500 text-2xl">Received Applications</span>
@@ -17,138 +17,42 @@
                         </br>
                         </br>
                         <!-- This is an example component -->
-                        <div class='mt-5'>
-                            <div
-                                class="rounded-xl border p-5 shadow-md w-12/12 bg-rgb(17 24 39 / var(--tw-bg-opacity, 1))">
+                        <div class='mt-6'>
+
+                            @foreach($applications as $application)
+
+                            <div class="rounded-2xl border p-5 mb-6 shadow-md w-10/12">
                                 <div class="flex w-full items-center justify-between border-b pb-3">
                                     <div class="flex items-center space-x-3">
                                         <div
                                             class="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]">
                                         </div>
-                                        <div class="text-lg font-bold">Joe Smith</div>
+                                        <div class="text-lg font-bold">{{ $application->user->name }}</div>
                                     </div>
                                     <div class="flex items-center space-x-8">
                                         <button
-                                            class="rounded-2xl border bg-neutral-700 px-3 py-1 text-xs font-semibold">id:
-                                            2</button>
-                                        <div class="text-xs">2025-08-28 11:59</div>
+                                            class="rounded-2xl border bg-neutral-700 px-3 py-1 text-xs font-semibold"># {{ $application->id}}</button>
+                                        <div class="text-xs">{{ $application->created_at }}</div>
                                     </div>
                                 </div>
-
                                 <div class="mt-4 mb-6">
-                                    <div class="mb-3 text-xl font-bold">Nulla sed leo tempus, feugiat velit vel, rhoncus
-                                        neque?</div>
-                                    <div class="text-sm">Aliquam a tristique sapien, nec bibendum urna.
-                                        Maecenas convallis dignissim turpis, non suscipit mauris interdum at. Morbi sed
-                                        gravida nisl, a pharetra nulla. Etiam tincidunt turpis leo, ut mollis ipsum
-                                        consectetur quis. Etiam faucibus est risus, ac condimentum mauris consequat nec.
-                                        Curabitur eget feugiat massa</div>
+                                    <div class="mb-3 text-xl font-bold">{{ $application->subject }}</div>
+                                    <div class="text-sm">{{ $application->message }}</div>
                                 </div>
 
                                 <div>
                                     <div class="flex items-center justify-between">
-                                        email@email.com
-
-
-
-                                        {{-- <div class="flex space-x-4 md:space-x-8">
-                                            <div
-                                                class="flex cursor-pointer items-center transition hover:text-slate-600">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="mr-1.5 h-5 w-5"
-                                                    fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                                                    stroke-width="2">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-                                                </svg>
-                                                <span>125</span>
-                                            </div>
-                                            <div
-                                                class="flex cursor-pointer items-center transition hover:text-slate-600">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="mr-1.5 h-5 w-5"
-                                                    fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                                                    stroke-width="2">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5" />
-                                                </svg>
-                                                <span>4</span>
-                                            </div>
-                                        </div> --}}
+                                        {{ $application->user->email }}
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        
+                        @endforeach
 
-                        <div class='mt-5'>
-                            <div
-                                class="rounded-xl border p-5 shadow-md w-12/12 bg-rgb(17 24 39 / var(--tw-bg-opacity, 1))">
-                                <div class="flex w-full items-center justify-between border-b pb-3">
-                                    <div class="flex items-center space-x-3">
-                                        <div
-                                            class="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]">
-                                        </div>
-                                        <div class="text-lg font-bold">Joe Smith</div>
-                                    </div>
-                                    <div class="flex items-center space-x-8">
-                                        <button
-                                            class="rounded-2xl border bg-neutral-700 px-3 py-1 text-xs font-semibold">id:
-                                            2</button>
-                                        <div class="text-xs">2025-08-28 11:59</div>
-                                    </div>
-                                </div>
-
-                                <div class="mt-4 mb-6">
-                                    <div class="mb-3 text-xl font-bold">Nulla sed leo tempus, feugiat velit vel, rhoncus
-                                        neque?</div>
-                                    <div class="text-sm">Aliquam a tristique sapien, nec bibendum urna.
-                                        Maecenas convallis dignissim turpis, non suscipit mauris interdum at. Morbi sed
-                                        gravida nisl, a pharetra nulla. Etiam tincidunt turpis leo, ut mollis ipsum
-                                        consectetur quis. Etiam faucibus est risus, ac condimentum mauris consequat nec.
-                                        Curabitur eget feugiat massa</div>
-                                </div>
-
-                                <div>
-                                    <div class="flex items-center justify-between">
-                                        email@email.com
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class='mt-5'>
-                            <div
-                                class="rounded-xl border p-5 shadow-md w-12/12 bg-rgb(17 24 39 / var(--tw-bg-opacity, 1))">
-                                <div class="flex w-full items-center justify-between border-b pb-3">
-                                    <div class="flex items-center space-x-3">
-                                        <div
-                                            class="h-8 w-8 rounded-full bg-slate-400 bg-[url('https://i.pravatar.cc/32')]">
-                                        </div>
-                                        <div class="text-lg font-bold">Joe Smith</div>
-                                    </div>
-                                    <div class="flex items-center space-x-8">
-                                        <button
-                                            class="rounded-2xl border bg-neutral-700 px-3 py-1 text-xs font-semibold">id:
-                                            2</button>
-                                        <div class="text-xs">2025-08-28 11:59</div>
-                                    </div>
-                                </div>
-
-                                <div class="mt-4 mb-6">
-                                    <div class="mb-3 text-xl font-bold">Nulla sed leo tempus, feugiat velit vel, rhoncus
-                                        neque?</div>
-                                    <div class="text-sm">Aliquam a tristique sapien, nec bibendum urna.
-                                        Maecenas convallis dignissim turpis, non suscipit mauris interdum at. Morbi sed
-                                        gravida nisl, a pharetra nulla. Etiam tincidunt turpis leo, ut mollis ipsum
-                                        consectetur quis. Etiam faucibus est risus, ac condimentum mauris consequat nec.
-                                        Curabitur eget feugiat massa</div>
-                                </div>
-
-                                <div>
-                                    <div class="flex items-center justify-between">
-                                        email@email.com
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        {{ $applications->links() }}
+                         
+                    </div>
                     @elseif(auth()->user()->role->name == 'client')
                         <div
                             class='flex items-center justify-center bg-rgb(17 24 39 / var(--tw-bg-opacity, 1))'>
@@ -156,18 +60,16 @@
                                 class='w-full max-w-lg px-10 py-8 mx-auto bg-rgb(17 24 39 / var(--tw-bg-opacity, 1)) rounded-lg shadow-xl border-2'>
                                 <div class='max-w-md mx-auto space-y-6'>
 
-                                    <form action="" method="POST" enctype="multipart/form-data">
+                                    <form action="{{ route('applications.store')}}" method="POST" enctype="multipart/form-data">
                                         @csrf
                                         <h2 class="text-2xl font-bold ">Submit your application</h2>
                                         <hr class="my-6">
-                                        <label class="uppercase text-sm font-bold opacity-70">Subject</label>
-                                        <input type="text" class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none">
-                                        <label class="uppercase text-sm font-bold opacity-70">Message</label>
-                                        <textarea rows="5" class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none"></textarea>
-                                        <label class="uppercase text-sm font-bold opacity-70">File</label>
-                                        <input type="file" class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none">
-                                        
-                                   
+                                        <label class="uppercase text-sm font-bold opacity-100">Subject</label>
+                                        <input type="text" name="subject" required class="p-3 mt-2 mb-4 w-full bg-slate-700 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none">
+                                        <label class="uppercase text-sm font-bold opacity-100">Message</label>
+                                        <textarea rows="5" name="message" required class="p-3 mt-2 mb-4 w-full bg-slate-700 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none"></textarea>
+                                        <label class="uppercase text-sm font-bold opacity-100">File</label>
+                                        <input type="file" name="file" class="p-3 mt-2 mb-4 w-full bg-slate-700 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none">
                                         <input type="submit"
                                             class="py-3 px-6 my-2 bg-emerald-500 text-white font-medium rounded hover:bg-indigo-500 cursor-pointer ease-in-out duration-300"
                                             value="Send">
